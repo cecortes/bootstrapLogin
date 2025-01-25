f1788d0a1f3086e301b0c519afaf65c19f275212
César López Cortés <cesarlopezcortes@hotmail.com>
2025-01-25 17:08:07 -0600

    Edit modal html done

--- app/docs/log.md
+++ app/docs/log.md
@@ -154,7 +154,8 @@
 
 - [x] Create the form to add a new company into a modal.
 - [x] Modify the styles of the modal.
-- [ ] Create the form to edit a company into a modal.
+- [x] Create the form to edit a company into a modal.
+- [x] Modify the styles of the modal.
 - [ ] Start the logic layer to add a new company.
 - [ ] Start the logic layer to edit a company.
 - [ ] Start the logic layer to delete a company.
--- app/js/company.js
+++ app/js/company.js
@@ -6,14 +6,20 @@ $(function () {
    * Dom elements
    */
   var $addCompanyModal = $("#modalAddCompany");
+  var $editCompanyModal = $("#modalEditCompany");
   var $addCompanyBtn = $("#add-btn");
+  var $editCompanyBtn = $("#edit-btn");
 
   /*
    * Event handlers
    */
   // Add company button
   $addCompanyBtn.click(function () {
-    console.log("Add company button clicked");
     $addCompanyModal.modal("show");
+  });
+
+  // Edit company button
+  $editCompanyBtn.click(function () {
+    $editCompanyModal.modal("show");
   });
 });
